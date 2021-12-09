12399421f46a68c8c80d616101ca33292ab195df
adrianoBP <18449283+adrianoBP@users.noreply.github.com>
2021-12-09 11:12:07 +0000

    Day 9: part 2

--- challanges/day-9.js
+++ challanges/day-9.js
@@ -8,15 +8,21 @@ const run = async () => {
         .map(row => row.split('').map(Number));
 
     const lowPoints = [];
+    const basins = [];
+
     for (let y = 0; y < rows.length; y++) {
         for (let x = 0; x < rows[y].length; x++) {
             if (isLowerThanTop(y, x, rows) && isLowerThanLeft(y, x, rows) && isLowerThanRight(y, x, rows) && isLowerThanBottom(y, x, rows)) {
                 lowPoints.push(rows[y][x]);
+
+                basins.push([...new Set(getBasin(x, y, rows))]);
             }
         }
     }
 
-    console.log(lowPoints.map(point => point + 1).reduce((a, b) => a + b));
+    const basinLengths = basins.map(basin => basin.length).sort((a, b) => b - a);
+
+    console.log(basinLengths[0] * basinLengths[1] * basinLengths[2]);
 };
 
 const isLowerThanTop = (y, x, rows) => {
@@ -53,6 +59,51 @@ const isLowerThanBottom = (y, x, rows) => {
         return true;
     }
     return rows[y][x] < rows[y + 1][x];
+}
+
+const getBasin = (x, y, rows, direction) => {
+
+    const basin = [
+        `${x},${y}`,
+        direction != 'bottom' ? getBasinWithDirection(x, y - 1, rows, 'top', rows[y][x]) : null,
+        direction != 'left' ? getBasinWithDirection(x + 1, y, rows, 'right', rows[y][x]) : null, ,
+        direction != 'top' ? getBasinWithDirection(x, y + 1, rows, 'bottom', rows[y][x]) : null, ,
+        direction != 'right' ? getBasinWithDirection(x - 1, y, rows, 'left', rows[y][x]) : null,
+    ]
+
+    return basin.flat().filter(point => point !== undefined && point !== null);
+}
+
+const getBasinWithDirection = (x, y, rows, direction, previousPoint) => {
+
+    if (direction === 'top') {
+
+        if (y < 0 || rows[y][x] == 9) return;
+
+        if (rows[y][x] >= previousPoint)
+            return getBasin(x, y, rows, direction);
+
+    } else if (direction === 'right') {
+
+        if (x >= rows[y].length || rows[y][x] == 9) return;
+
+        if (rows[y][x] >= previousPoint)
+            return getBasin(x, y, rows, direction);
+
+    } else if (direction === 'bottom') {
+
+        if (y >= rows.length || rows[y][x] == 9) return;
+
+        if (rows[y][x] >= previousPoint)
+            return getBasin(x, y, rows, direction);
+
+    } else if (direction === 'left') {
+
+        if (x < 0 || rows[y][x] == 9) return;
+
+        if (rows[y][x] >= previousPoint)
+            return getBasin(x, y, rows, direction);
+    }
 }
 
 run();
