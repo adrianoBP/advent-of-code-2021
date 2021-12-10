58c16a76d96e903b12ee36e8ff412fe95ecb0984
adrianoBP <18449283+adrianoBP@users.noreply.github.com>
2021-12-10 09:06:40 +0000

    Day 10: part 2

--- challanges/day-10.js
+++ challanges/day-10.js
@@ -6,62 +6,59 @@ const run = async () => {
         .toLocaleString()
         .split('\n');
 
-
-    let counter = 0;
+    let totalPointsByRow = [];
     for (let row of rows) {
 
         const stack = [];
+        let errorFound = false;
 
         for (let char of row.split('')) {
-
-            let errorFound = false;
 
             if (['(', '[', '{', '<'].includes(char)) {
                 stack.push(char);
             } else {
                 switch (char) {
 
-                    case ')': {
-                        if (stack.pop() !== '(') {
-                            counter += 3;
-                            errorFound = true;
-                        }
+                    case ')':
+                        if (stack.pop() !== '(') errorFound = true;
                         break;
-                    }
 
-                    case ']': {
-                        if (stack.pop() !== '[') {
-                            counter += 57;
-                            errorFound = true;
-                        }
+                    case ']':
+                        if (stack.pop() !== '[') errorFound = true;
                         break;
-                    }
 
-                    case '}': {
-                        if (stack.pop() !== '{') {
-                            counter += 1197;
-                            errorFound = true;
-                        }
+                    case '}':
+                        if (stack.pop() !== '{') errorFound = true;
                         break;
-                    }
 
-                    case '>': {
-                        if (stack.pop() !== '<') {
-                            counter += 25137;
-                            errorFound = true;
-                        }
+                    case '>':
+                        if (stack.pop() !== '<') errorFound = true;
                         break;
-                    }
-
                 }
             }
-
-            if (errorFound) break;
         }
+
+        if (errorFound || stack.length == 0) continue;
+
+        let counter = 0;
+        for (let char of stack.reverse()) {
+            let value = getCharacterValue(char);
+            counter = (counter * 5) + value;
+        }
+        totalPointsByRow.push(counter);
     }
 
-    console.log(counter);
-
+    totalPointsByRow.sort((a, b) => a - b);
+    console.log(totalPointsByRow[Math.floor(totalPointsByRow.length / 2)]);
 };
+
+const getCharacterValue = (char) => {
+    switch (char) {
+        case '(': return 1;
+        case '[': return 2;
+        case '{': return 3;
+        case '<': return 4;
+    }
+}
 
 run();
