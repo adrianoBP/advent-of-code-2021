890724a17de1115e57f298c3a749f6da65bddfc6
adrianoBP <18449283+adrianoBP@users.noreply.github.com>
2021-12-11 14:57:23 +0000

    Day 11: part 1

---
+++ challanges/day-11.js
@@ -0,0 +1,127 @@
+const fs = require('fs');
+
+const run = async () => {
+
+    const octopuses = fs.readFileSync(`${__dirname}/../inputs/day-11-input.txt`)
+        .toLocaleString()
+        .split('\n')
+        .map(row => row.split('')
+            .map(Number));
+
+    let allFlashes = 0;
+    for (let iterations = 0; iterations < 100; iterations++) {
+
+        // Step 1: increase all values by 1
+        for (let y = 0; y < octopuses.length; y++) {
+            for (let x = 0; x < octopuses[y].length; x++) {
+                octopuses[y][x] += 1;
+            }
+        }
+
+        // Step 2: Flash octopuses
+        let flashed = [];
+        for (let y = 0; y < octopuses.length; y++) {
+            for (let x = 0; x < octopuses[y].length; x++) {
+                flashOctopuses(octopuses, x, y, flashed);
+            }
+        }
+
+        // Step 2.1: count all flashes
+        for (let y = 0; y < octopuses.length; y++) {
+            for (let x = 0; x < octopuses[y].length; x++) {
+                if (octopuses[y][x] > 9) {
+                    allFlashes++;
+                }
+            }
+        }
+
+        // Step 3: Set flashed to 0
+        for (let y = 0; y < octopuses.length; y++) {
+            for (let x = 0; x < octopuses[y].length; x++) {
+                if (octopuses[y][x] > 9) {
+                    octopuses[y][x] = 0;
+                }
+            }
+        }
+    }
+
+    console.log(allFlashes);
+};
+
+const flashOctopuses = (octopuses, x, y, flashed) => {
+
+    if (flashed.includes(`${x},${y}`)) return;
+
+    const octopus = {
+        x: parseInt(x),
+        y: parseInt(y),
+    }
+
+    if (octopuses[octopus.y][octopus.x] > 9) {
+
+        flashed.push(`${octopus.x},${octopus.y}`);
+        const newFlashed = flashSurrounding(octopuses, octopus.x, octopus.y);
+
+        for (let i = 0; i < newFlashed.length; i++) {
+            flashOctopuses(octopuses, newFlashed[i].split(',')[0], newFlashed[i].split(',')[1], flashed);
+        }
+    }
+};
+
+const flashSurrounding = (octopuses, x, y) => {
+
+    const newFlashed = [];
+
+    // top-left
+    if (y - 1 >= 0 && x - 1 >= 0) {
+        octopuses[y - 1][x - 1]++;
+        newFlashed.push(`${x - 1},${y - 1}`);
+    }
+
+    // top
+    if (y - 1 >= 0) {
+        octopuses[y - 1][x]++;
+        newFlashed.push(`${x},${y - 1}`);
+    }
+
+    // top-right
+    if (y - 1 >= 0 && x + 1 < octopuses[y].length) {
+        octopuses[y - 1][x + 1]++;
+        newFlashed.push(`${x + 1},${y - 1}`);
+    }
+
+    // right
+    if (x + 1 < octopuses[y].length) {
+        octopuses[y][x + 1]++;
+        newFlashed.push(`${x + 1},${y}`);
+    }
+
+    // bottom-right
+    if (y + 1 < octopuses.length && x + 1 < octopuses[y].length) {
+        octopuses[y + 1][x + 1]++;
+        newFlashed.push(`${x + 1},${y + 1}`);
+    }
+
+    // bottom
+    if (y + 1 < octopuses.length) {
+        octopuses[y + 1][x]++;
+        newFlashed.push(`${x},${y + 1}`);
+    }
+
+    // bottom-left
+    if (y + 1 < octopuses.length && x - 1 >= 0) {
+        octopuses[y + 1][x - 1]++;
+        newFlashed.push(`${x - 1},${y + 1}`);
+    }
+
+    // left
+    if (x - 1 >= 0) {
+        octopuses[y][x - 1]++;
+        newFlashed.push(`${x - 1},${y}`);
+    }
+
+    return newFlashed;
+}
+
+
+run();
